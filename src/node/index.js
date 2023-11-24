7d51bca8b9eab0fb8a3797a28deee2327b330b4e
rina deguchi <rina.deguchi@rakus-partners.co.jp>
2023-11-24 17:48:00 +0900

    QAマラソン12kmまでの変更

--- src/node/index.js
+++ src/node/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const port = 自分の社員番号;
+const port = 3322;
 
 const cors = require("cors");
 app.use(cors());
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "x", // PostgreSQLのユーザー名に置き換えてください
-  host: "localhost",
-  database: "x", // PostgreSQLのデータベース名に置き換えてください
-  password: "x", // PostgreSQLのパスワードに置き換えてください
+  user: "user_3322", // PostgreSQLのユーザー名に置き換えてください
+  host: "db",
+  database: "crm_3322", // PostgreSQLのデータベース名に置き換えてください
+  password: "pass_3322", // PostgreSQLのパスワードに置き換えてください
   port: 5432,
 });
 
@@ -37,7 +37,7 @@ app.post("/add-customer", async (req, res) => {
   try {
     const { companyName, industry, contact, location } = req.body;
     const newCustomer = await pool.query(
-      "INSERT INTO customers (company_nam, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
+      "INSERT INTO customers (company_name, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
       [companyName, industry, contact, location]
     );
     res.json({ success: true, customer: newCustomer.rows[0] });
@@ -48,3 +48,63 @@ app.post("/add-customer", async (req, res) => {
 });
 
 app.use(express.static("public"));
+
+// 既存のコードに追加
+app.get("/customer/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    const customerData = await pool.query("SELECT * FROM customers WHERE customer_id = $1", [customerId]);
+
+    if (customerData.rows.length > 0) {
+      res.json(customerData.rows[0]);
+    } else {
+      res.status(404).send("Customer not found");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// 既存のコードに追加
+app.delete("/customer/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    console.log(`Deleting customer with ID: ${customerId}`);
+    const deletedCustomer = await pool.query("DELETE FROM customers WHERE customer_id = $1 RETURNING *", [customerId]);
+
+    if (deletedCustomer.rows.length > 0) {
+      res.json({ success: true, customer: deletedCustomer.rows[0] });
+    } else {
+      res.status(404).send("Customer not found");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// 既存のコードに追加
+app.put("/customer/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    console.log("PUT request to /customer/:customerId");
+    // リクエストボディからデータを取得
+    const { companyName, industry, contact, location } = req.body;
+
+    // ここでデータベースの customers テーブルを更新するクエリを実行
+    const updatedCustomer = await pool.query(
+      "UPDATE customers SET company_name = $1, industry = $2, contact = $3, location = $4 WHERE customer_id = $5 RETURNING *",
+      [companyName, industry, contact, location, customerId]
+    );
+
+    if (updatedCustomer.rows.length > 0) {
+      res.json({ success: true, customer: updatedCustomer.rows[0] });
+    } else {
+      res.status(404).send("Customer not found");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
